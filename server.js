a1008ff2eb89173ea567b54dcb42774b8e95b9a7
Aslak2322 <147713237+Aslak2322@users.noreply.github.com>
2025-10-08 08:29:19 +0200

    Render path 2

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.post("/checkout", auth, async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.use(express.static(path.join(__dirname, 'build')));
-  app.get('/:path(*)', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
